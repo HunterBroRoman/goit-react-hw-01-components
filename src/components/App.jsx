23a706e142ea6899ea083f3345030a38b83849ff
Roman <sayga76239@gmail.com>
2023-01-30 12:30:18 +0100

    profiles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
-import Profile from './Profile/Profile';
+import { ProfileCard } from './Profile/Profile';
+import user from './Profile/user.json';
 
 export const App = () => {
   return (
-    <div>
-      
-    </div>
+    <div
+      style={{
+        height: '100%',
+        display: 'block',
+        color: '#010101',
+      }}
+    >
+      <ProfileCard
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    
+  </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,32 @@
+
+// import PropTypes from 'prop-types';
+import styles from './profile.css';
+
+
+export function ProfileCard({ avatar, location, tag, username, stats }) {
+    return ( 
+<div className={styles.profile}>
+      <div className="description">
+        <img src={avatar} alt="User avatar" className={styles.avatar} />
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li className={styles.stats_item}>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li className={styles.stats_item}>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li className={styles.stats_item}>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+    );
+}
